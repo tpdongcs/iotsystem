99210e8e110373050336b9ff222da08dcb0f1264
HNguyen\HuyNho <nguyenquanghuy031295@gmail.com>
2017-04-05 12:47:49 +0700

    add controller for post ECG add object ECG Data

---
+++ src/main/java/hcmut/edu/model/ECGObject.java
@@ -0,0 +1,14 @@
+package hcmut.edu.model;
+
+/**
+ * Created by HuyNho on 4/4/2017.
+ */
+public class ECGObject {
+    private double[] ECGarr;
+
+    public double[] getECGarr() {return ECGarr;}
+
+    public void setECGarr(double[] ECGarr) {
+        this.ECGarr = ECGarr;
+    }
+}
